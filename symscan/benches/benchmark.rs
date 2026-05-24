cad03d88c15bf193488e8e7334b0b43e26dda147
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2026-05-25 08:18:50 +0900

    feat: add hamming benchmarks

--- symscan/benches/benchmark.rs
+++ symscan/benches/benchmark.rs
@@ -1,6 +1,9 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use std::io::{self, BufRead, Cursor};
-use symscan::{get_neighbors_across, get_neighbors_within, CachedRef};
+use symscan::{
+    get_hamming_neighbors_across, get_hamming_neighbors_within, get_neighbors_across,
+    get_neighbors_within, CachedRef, CachedRefHamming,
+};
 
 static QUERY_BYTES: &[u8] = include_bytes!("../../test_files/cdr3b_10k_a.txt");
 static REFERENCE_BYTES: &[u8] = include_bytes!("../../test_files/cdr3b_10k_b.txt");
@@ -17,34 +20,66 @@ fn setup_benchmarks(c: &mut Criterion) {
     let reference = bytes_as_ascii_lines(REFERENCE_BYTES);
     let cached_query = CachedRef::new(&query, 1).expect("short input");
     let cached_reference = CachedRef::new(&reference, 1).expect("short input");
+    let cached_query_hamming = CachedRefHamming::new(&query, 1).expect("short input");
+    let cached_reference_hamming = CachedRefHamming::new(&reference, 1).expect("short input");
 
-    c.bench_function("get_candidates_within", |b| {
+    c.bench_function("get_neighbors_within", |b| {
         b.iter(|| {
             let _ = get_neighbors_within(&query, 1);
         })
     });
 
-    c.bench_function("get_candidates_cross", |b| {
+    c.bench_function("get_hamming_neighbors_within", |b| {
+        b.iter(|| {
+            let _ = get_hamming_neighbors_within(&query, 1);
+        })
+    });
+
+    c.bench_function("get_neighbors_across", |b| {
         b.iter(|| {
             let _ = get_neighbors_across(&query, &reference, 1);
         })
     });
 
-    c.bench_function("get_candidates_within (cached)", |b| {
+    c.bench_function("get_hamming_neighbors_across", |b| {
+        b.iter(|| {
+            let _ = get_hamming_neighbors_across(&query, &reference, 1);
+        })
+    });
+
+    c.bench_function("get_neighbors_within (cached)", |b| {
         b.iter(|| {
             let _ = cached_reference.get_neighbors_within(1);
         })
     });
 
-    c.bench_function("get_candidates_cross (partially cached)", |b| {
+    c.bench_function("get_hamming_neighbors_within (cached)", |b| {
+        b.iter(|| {
+            let _ = cached_reference_hamming.get_neighbors_within(1);
+        })
+    });
+
+    c.bench_function("get_neighbors_cross (partially cached)", |b| {
         b.iter(|| {
             let _ = cached_reference.get_neighbors_across(&query, 1);
         })
     });
 
-    c.bench_function("get_candidates_cross (fully cached)", |b| {
+    c.bench_function("get_neighbors_cross (fully cached)", |b| {
         b.iter(|| {
             let _ = cached_reference.get_neighbors_across_cached(&cached_query, 1);
+        })
+    });
+
+    c.bench_function("get_hamming_neighbors_cross (partially cached)", |b| {
+        b.iter(|| {
+            let _ = cached_reference_hamming.get_neighbors_across(&query, 1);
+        })
+    });
+
+    c.bench_function("get_hamming_neighbors_cross (fully cached)", |b| {
+        b.iter(|| {
+            let _ = cached_reference_hamming.get_neighbors_across_cached(&cached_query_hamming, 1);
         })
     });
 
